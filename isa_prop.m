fd3c38f3fefa8b95f87d292f38770bdfe44c261e
Ahmed Rashed <ahmed.m.rashed@gmail.com>
2024-04-05 17:07:23 +0200

    Improve and condense

--- isa_prop.m
+++ isa_prop.m
@@ -13,14 +13,14 @@ p_0_row=nan(1,N_layers);
 p_0_row(1)=101330;
 
 % Fill in T_0_row and p_0_row
-for n=1:N_layers-1
-    if mod(n,2)~=0  %n is odd ==> gradient layer
-        [T_0_row(n+1),p_0_row(n+1)]=graient_T_p(h_G0_row(n+1),h_G0_row(n),T_0_row(n),p_0_row(n),a_0_row((n+1)/2));
-    else    %n is even ==> isothermal layer
-        [T_0_row(n+1),p_0_row(n+1)]=isothermal_T_p(h_G0_row(n+1),h_G0_row(n),T_0_row(n),p_0_row(n));
+for n_layer=1:N_layers-1
+    if mod(n_layer,2)~=0  %n_layer is odd ==> gradient layer
+        [T_0_row(n_layer+1),p_0_row(n_layer+1)]=graient_T_p(h_G0_row(n_layer+1),n_layer);
+    else    %n_layer is even ==> isothermal layer
+        [T_0_row(n_layer+1),p_0_row(n_layer+1)]=isothermal_T_p(h_G0_row(n_layer+1),n_layer);
     end
 end
-% [T_0_row.',p_0_row.']
+[T_0_row.',p_0_row.'] %#ok<NOPRT> 
 
 h_vec=r.*h_G_vec./(r+h_G_vec);
 T_vec=nan(size(h_G_vec));
@@ -31,12 +31,12 @@ for n=1:numel(h_G_vec)
         warning("Invalid value in the input. Values must lie in the region ["+h_G0_row(1)+","+h_G0_row(end)+"]");
     end
     
-    for m=2:8
-        if h_G_vec(n)<=h_G0_row(m)
-            if mod(m,2)==0  %m is even ==> gradient layer
-                [T_vec(n),p_vec(n)]=graient_T_p(h_G_vec(n),h_G0_row(m-1),T_0_row(m-1),p_0_row(m-1),a_0_row(m/2));
-            else    %m is odd ==> isothermal layer
-                [T_vec(n),p_vec(n)]=isothermal_T_p(h_G_vec(n),h_G0_row(m-1),T_0_row(m-1),p_0_row(m-1));
+    for n_layer=1:N_layers
+        if h_G_vec(n)<=h_G0_row(n_layer+1)
+            if mod(n_layer,2)~=0  %n_layer is odd ==> gradient layer
+                [T_vec(n),p_vec(n)]=graient_T_p(h_G_vec(n),n_layer);
+            else    %n_layer is even ==> isothermal layer
+                [T_vec(n),p_vec(n)]=isothermal_T_p(h_G_vec(n),n_layer);
             end
 
             break
@@ -46,13 +46,13 @@ end
 rho_vec=p_vec./R./T_vec;
 a_vec=sqrt(gamma.*R.*T_vec);
 
-    function [T,p]=graient_T_p(h_G,h_G0,T_0,p_0,a_0)
-        T=T_0+a_0.*(h_G-h_G0);
-        p=p_0.*(T./T_0).^(-g_0./a_0./R);
+    function [T,p]=graient_T_p(h_G,n_layer)
+        T=T_0_row(n_layer)+a_0_row((n_layer+1)/2).*(h_G-h_G0_row(n_layer));
+        p=p_0_row(n_layer).*(T./T_0_row(n_layer)).^(-g_0./a_0_row((n_layer+1)/2)./R);
     end
     
-    function [T,p]=isothermal_T_p(h_G,h_G0,T_0,p_0)
-        T=T_0;
-        p=p_0.*exp(-g_0.*(h_G-h_G0)./R./T_0);
+    function [T,p]=isothermal_T_p(h_G,n_layer)
+        T=T_0_row(n_layer);
+        p=p_0_row(n_layer).*exp(-g_0.*(h_G-h_G0_row(n_layer))./R./T_0_row(n_layer));
     end
 end
